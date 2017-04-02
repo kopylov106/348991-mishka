cd8f04d270db4f34687c59815a41494d9ec8dd0b
Dmitriy Kopylov <youyoukin@gmail.com>
2017-04-02 15:30:36 +0300

    небольшие тестовые улчшения

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 "use strict";
 
 var gulp = require("gulp");
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
 var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+
+gulp.task("symbols", function() {
+  return gulp.src("img/active-icons/*.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("symbols.svg"))
+    .pipe(gulp.dest("img"));
+});
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
@@ -28,6 +41,16 @@ gulp.task("serve", ["style"], function() {
     cors: true,
     ui: false
   });
+
+gulp.task("sprite", function() {
+  return gulp.src("img/active-icons/*.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("img"));
+});
 
   gulp.watch("less/**/*.less", ["style"]);
   gulp.watch("*.html").on("change", server.reload);
